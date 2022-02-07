2b837f191289b02f0ad103a157d2e6b5d827a8b2
MOSSAD <tsukikagemaikaka1@gmail.com>
2022-02-07 22:56:54 +0700

    change to compressed images

--- modules/docs.js
+++ modules/docs.js
@@ -1,22 +1,22 @@
 const docs = [
   {
     link: "https://hultprize-dashboard-15nlnq5uy-bazoka-kaka.vercel.app/",
-    img: "images/hultprizeits site.png",
+    img: "images/hultprizeits site-min.png",
     desc: "Hultprize at ITS",
   },
   {
     link: "https://schematics.vercel.app/",
-    img: "images/schematics.PNG",
+    img: "images/schematics-min.PNG",
     desc: "Schematics ITS 2021",
   },
   {
     link: "https://cofilm.id/",
-    img: "images/cofilm.PNG",
+    img: "images/cofilm-min.PNG",
     desc: "Cofilm+ Indonesia",
   },
   {
     link: "https://tindog-bazoka-kaka.netlify.app/",
-    img: "images/tindog.png",
+    img: "images/tindog-min.png",
     desc: "Tindog App",
   },
   {
@@ -26,32 +26,32 @@ const docs = [
   },
   {
     link: "https://codepen.io/bazoka-kaka/full/vYeLqLo",
-    img: "images/guessing game.png",
+    img: "images/guessing game-min.png",
     desc: "Guessing Game",
   },
   {
     link: "https://simon-game-bazokakaka.netlify.app/",
-    img: "images/simon game.PNG",
+    img: "images/simon game-min.PNG",
     desc: "Simon Game",
   },
   {
     link: "https://judaistic.netlify.app/",
-    img: "images/judaistic101.PNG",
+    img: "images/judaistic101-min.PNG",
     desc: "Judaistic101",
   },
   {
     link: "https://codepen.io/bazoka-kaka/full/QWMJLXz",
-    img: "images/docs page.PNG",
+    img: "images/docs page-min.PNG",
     desc: "JS Docs Page",
   },
   {
     link: "https://drumkit-bazoka-kaka.netlify.app/",
-    img: "images/drum.PNG",
+    img: "images/drum-min.PNG",
     desc: "Drum Kit",
   },
   {
     link: "https://blogpost-bazokakaka.herokuapp.com/",
-    img: "images/blog.PNG",
+    img: "images/blog-min.PNG",
     desc: "Blog Compose",
   },
   {
