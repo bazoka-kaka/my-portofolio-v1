d2365b138e6f564ce423700a5edaad234d553687
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2022-11-28 02:48:20 +0700

    comment out projects

--- modules/docs.js
+++ modules/docs.js
@@ -14,51 +14,51 @@ const docs = [
     img: "images/cofilm-min.png",
     desc: "Cofilm+ Indonesia",
   },
-  {
-    link: "https://todolist-bazokakaka.herokuapp.com/",
-    img: "images/todolist.png",
-    desc: "Todolist",
-  },
-  {
-    link: "https://cia-application.netlify.app/",
-    img: "images/cia-capture-min.png",
-    desc: "CIA Application",
-  },
-  {
-    link: "https://questions-app-bazokakaka.herokuapp.com/",
-    img: "images/form-min.png",
-    desc: "Question App (Politics)",
-  },
-  {
-    link: "https://judaistic.netlify.app/",
-    img: "images/judaistic101-min.png",
-    desc: "Judaistic101",
-  },
-  {
-    link: "https://codepen.io/bazoka-kaka/full/vYeLqLo",
-    img: "images/guessing game-min.png",
-    desc: "Guessing Game",
-  },
-  {
-    link: "https://codepen.io/bazoka-kaka/full/QWMJLXz",
-    img: "images/docs page-min.png",
-    desc: "JS Docs Page",
-  },
-  {
-    link: "https://blogpost-bazokakaka.herokuapp.com/",
-    img: "images/blog-min.png",
-    desc: "Blog Compose",
-  },
-  {
-    link: "https://codepen.io/bazoka-kaka/full/mdMzwyL",
-    img: "images/albert einstein-min.png",
-    desc: "Tribute Page",
-  },
-  {
-    link: "https://codepen.io/bazoka-kaka/full/bGrmapE",
-    img: "images/kosher food-min.jpg",
-    desc: "Indonesian Kosher",
-  },
+  // {
+  //   link: "https://todolist-bazokakaka.herokuapp.com/",
+  //   img: "images/todolist.png",
+  //   desc: "Todolist",
+  // },
+  // {
+  //   link: "https://cia-application.netlify.app/",
+  //   img: "images/cia-capture-min.png",
+  //   desc: "CIA Application",
+  // },
+  // {
+  //   link: "https://questions-app-bazokakaka.herokuapp.com/",
+  //   img: "images/form-min.png",
+  //   desc: "Question App (Politics)",
+  // },
+  // {
+  //   link: "https://judaistic.netlify.app/",
+  //   img: "images/judaistic101-min.png",
+  //   desc: "Judaistic101",
+  // },
+  // {
+  //   link: "https://codepen.io/bazoka-kaka/full/vYeLqLo",
+  //   img: "images/guessing game-min.png",
+  //   desc: "Guessing Game",
+  // },
+  // {
+  //   link: "https://codepen.io/bazoka-kaka/full/QWMJLXz",
+  //   img: "images/docs page-min.png",
+  //   desc: "JS Docs Page",
+  // },
+  // {
+  //   link: "https://blogpost-bazokakaka.herokuapp.com/",
+  //   img: "images/blog-min.png",
+  //   desc: "Blog Compose",
+  // },
+  // {
+  //   link: "https://codepen.io/bazoka-kaka/full/mdMzwyL",
+  //   img: "images/albert einstein-min.png",
+  //   desc: "Tribute Page",
+  // },
+  // {
+  //   link: "https://codepen.io/bazoka-kaka/full/bGrmapE",
+  //   img: "images/kosher food-min.jpg",
+  //   desc: "Indonesian Kosher",
+  // },
 ];
 
 module.exports = docs;
